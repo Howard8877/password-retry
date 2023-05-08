364eb679b858d9aedbfecfa3d69b68021430b2d4
Howard <pupo87009@gmail.com>
2023-05-08 22:18:23 +0800

    change version

--- password.py
+++ password.py
@@ -1,15 +1,18 @@
 password = input('請設定密碼:')
-x = 2
-while x >= 0:
+x = 3
+while x > 0:
+	x = x - 1
 	password1 = input('請輸入剛才設定的密碼:')
 	if password == password1:
 		print('登入成功!')
 		break
 	else:
-		if x == 0:
-			break
-		print('密碼錯誤! 還有', x, '次機會')
-		x = x - 1
+		print('密碼錯誤!')
+		if x > 0:
+			print('還有', x, '次機會')
+		else:
+		    print('被鎖帳號!')	
+
 
 
 
